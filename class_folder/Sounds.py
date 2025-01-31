33a8c6c43cb9eadbf6770433cc49b2f4d276f2ba
AeRiaSaito <florence.navet@laplateforme.io>
2025-01-31 13:45:50 +0100

    feat: imported slice fruit sound, bomb explosion sound freeze sound

--- class_folder/Sounds.py
+++ class_folder/Sounds.py
@@ -1,33 +1,20 @@
 import pygame
-from __settings__ import SOUND_BOMB, SOUND_FREEZE
 
 class Sounds:
     def __init__(self):
-        self.explose_sound = pygame.mixer.Sound("SOUND_BOMB")
-        self.freeze_sound = pygame.mixer.Sound("SOUND_FREEZE")
-        self.music_volume = pygame.mixer.set_voluem(0.25)
-        self.volume()
+        pygame.mixer.init()
+        self.slice_sound = pygame.mixer.Sound("./assets/sounds/slice_01.wav")
+        self.slice_sound.set_volume(0.5)
+        self.bomb_sound = pygame.mixer.Sound("./assets/sounds/explosion-2.wav")
+        self.bomb_sound.set_volume(0.5)
+        self.freeze_sound = pygame.mixer.Sound("./assets/sounds/freeze.wav")
+        self.bomb_sound.set_volume(0.5)
 
-    def sound_volume(self):
-        self.explose_sound.set_volume(0.15)
-        self.explose_sound.set_volume(0.05)
+    def play_slice_sound(self):
+        self.slice_sound.play()
 
+    def play_bomb_sound(self):
+        self.bomb_sound.play()
 
-
-"""
-faudra rajouter ça la liste slice_sound.pay() sous fruit.pop(index)
-   for fruit in fruits:
-        if event.key == pygame.key.key_code(letter):
-            if len(fruits) > 0:
-                if fruit.letter.lower() == letter:
-                    index = fruits.index(fruit)
-                
-                    fruit_slices_1 = Fruit_slices(fruit.x, fruit.y, fruit.width, fruit.name, screen.screen)
-                    fruit_slices_2 = Fruit_slices(fruit.x, fruit.y, fruit.width, fruit.name, screen.screen)
-                    fruits_slices.append(fruit_slices_1)
-                    fruits_slices.append(fruit_slices_2)
-                    fruits.pop(index)
-
-                
-                    slice_sound.play()
-"""
+    def play_freeze_sound(self):
+        self.freeze_sound.play()
