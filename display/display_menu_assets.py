1aacd070c4410b4cf1a72aa40eb8d2274533377c
Adeline <adeline.patenne@hotmail.fr>
2025-01-31 13:42:20 +0100

    fix: first attempt hovered menu item in mode menu

--- display/display_menu_assets.py
+++ display/display_menu_assets.py
@@ -1,7 +1,8 @@
 import pygame,json, os
 from class_folder.Screen import Screen
-from __settings__ import STYLE_FONT, MAIN_FONT, BUTTON_IMAGE, ASSETS_DICT, SCORE_PATH, TEXT_COLOR, TEXT_COLOR_LIGHT
+from __settings__ import STYLE_FONT, MAIN_FONT, BUTTON_IMAGE, ASSETS_DICT, SCORE_PATH, TEXT_COLOR, TEXT_COLOR_LIGHT, BACKGROUND_IMAGE_MENU
 from class_folder.Button import Button
+from class_folder.Button_simple import Button_simple
 
 BUTTON_IMAGE = pygame.image.load(ASSETS_DICT["plank2"])
 HEART_IMAGE = pygame.image.load(ASSETS_DICT["heart"])
@@ -9,6 +10,7 @@ HEART_IMAGE = pygame.image.load(ASSETS_DICT["heart"])
 width = 1200
 height = 720
 screen = Screen(1080, 720)
+
 
 def display_hearts(life, strike):
     hearts = []
@@ -61,48 +63,99 @@ def load_scores():
         scores = json.load(file)
     return scores
 
-def display_mode_menu(game_mode, language):
+
+def create_mode_menu_button():
     in_menu_button = Button(450, 100, "Mode", "mode_menu", 2, ((screen.width // 2), 70))
     in_menu_button.draw(TEXT_COLOR)
-
-    #  width, height, text, identification, flip, center
     difficulty_button = Button(300, 70, "Difficulté", "mode_menu", 1, ((screen.width // 4), (screen.height // 2 - 100)))
-
     language_button = Button(300, 70, "Langue", "mode_menu", 3, ((screen.width // 4)*3, (screen.height // 2 - 100)))
     difficulty_button.draw(TEXT_COLOR)
     language_button.draw(TEXT_COLOR)
 
-    
-    easy_difficulty_button = Button(250, 50, "Facile", "easy_mode", 2, ((screen.width // 4), (screen.height // 2 - 10)))
-    normal_difficulty_button = Button(250, 50, "Normal", "normal_mode", 3, ((screen.width // 4), (screen.height // 2 + 70)))
-    nightmare_difficulty_button = Button(250, 50, "Nightmare", "nightmare_mode", 3, ((screen.width // 4), (screen.height // 2 + 150)))
-    french_button = Button(250, 50, "Français", "french_mode", 4, ((screen.width // 4)*3, (screen.height // 2 - 10)))
-    english_button = Button(250, 50, "Anglais", "english_mode", 1, ((screen.width // 4)*3, (screen.height // 2 + 70)))
-    button_list = [easy_difficulty_button, normal_difficulty_button, nightmare_difficulty_button]
-    language_list = [french_button, english_button]
-    
-    easy_difficulty_button.draw(TEXT_COLOR)
-    normal_difficulty_button.draw(TEXT_COLOR)
-    nightmare_difficulty_button.draw(TEXT_COLOR)
 
+def display_mode_menu(game_mode, language):
+    create_mode_menu_button()
+    
+
+    #  width, height, text, identification, flip, center
+    
+
+    
+    # easy_difficulty_button = Button(250, 50, "Facile", "easy_mode", 2, ((screen.width // 4), (screen.height // 2 - 10)))
+    # normal_difficulty_button = Button(250, 50, "Normal", "normal_mode", 3, ((screen.width // 4), (screen.height // 2 + 70)))
+    # nightmare_difficulty_button = Button(250, 50, "Nightmare", "nightmare_mode", 3, ((screen.width // 4), (screen.height // 2 + 150)))
     # french_button = Button(250, 50, "Français", "french_mode", 4, ((screen.width // 4)*3, (screen.height // 2 - 10)))
     # english_button = Button(250, 50, "Anglais", "english_mode", 1, ((screen.width // 4)*3, (screen.height // 2 + 70)))
-    french_button.draw(TEXT_COLOR)
-    english_button.draw(TEXT_COLOR)
+    # easy_difficulty_button.draw(TEXT_COLOR)
+    # normal_difficulty_button.draw(TEXT_COLOR)
+    # nightmare_difficulty_button.draw(TEXT_COLOR)
+    # french_button.draw(TEXT_COLOR)
+    # english_button.draw(TEXT_COLOR)
 
+    #text, identification, font, font_size, color
+
+    easy_difficulty_button = Button_simple("Facile", "easy_mode", 28, screen.screen, ((screen.width // 4), (screen.height // 2 - 10)))
+    normal_difficulty_button = Button_simple("Normal", "normal_mode", 28, screen.screen, (screen.width // 4, screen.height // 2 + 70))
+    nightmare_difficulty_button = Button_simple("Nighmare", "nightmare_mode", 28, screen.screen, (screen.width // 4, screen.height // 2 + 150))
+    french_button = Button_simple("Français", "french_mode", 28, screen.screen, ((screen.width // 4)*3, screen.height // 2 - 10))
+    english_button = Button_simple("Anglais", "english_mode", 28, screen.screen, ((screen.width // 4)*3, screen.height // 2 + 70))
+
+    easy_difficulty_button.text_render(MAIN_FONT, "white")
+    normal_difficulty_button.text_render(MAIN_FONT, "white")
+    nightmare_difficulty_button.text_render(MAIN_FONT, "white")
+
+    french_button.text_render(MAIN_FONT, "white")
+    english_button.text_render(MAIN_FONT, "white")
+                                            
+
+    # dialog_render("Facile", MAIN_FONT, 32, "white")
+    # easy_difficulty_rect = easy_difficulty_button.get_rect(center = ((screen.width // 4), (screen.height // 2 - 10)))
+    # screen.screen.blit(easy_difficulty_button, easy_difficulty_rect)
+
+    # normal_difficulty_button = dialog_render("Normal", MAIN_FONT, 32, "white")
+    # normal_difficulty_rect = normal_difficulty_button.get_rect(center = ((screen.width // 4), (screen.height // 2 + 70)))
+    # screen.screen.blit(normal_difficulty_button, normal_difficulty_rect)
+
+    # nightmare_difficulty_button = dialog_render("Nightmare", MAIN_FONT, 32, "white")
+    # nightmare_rect = nightmare_difficulty_button.get_rect(center = ((screen.width // 4), (screen.height // 2 + 150)))
+    # screen.screen.blit(nightmare_difficulty_button, nightmare_rect)
+
+    # french_button = dialog_render("Français", MAIN_FONT, 32, "white")
+    # french_rect = french_button.get_rect(center = ((screen.width // 4)*3, (screen.height // 2 - 10)))
+    # screen.screen.blit(french_button, french_rect)
+
+    # english_button = dialog_render("Anglais", MAIN_FONT, 32, "white")
+    # english_rect = english_button.get_rect(center = ((screen.width // 4)*3, (screen.height // 2 + 70)))
+    # screen.screen.blit(english_button, english_rect)
+
+
+
+
+    # button_return = dialog_render("RETOUR", MAIN_FONT, 32, "white")
+    # button_return_rect = button_return.get_rect(center = (screen.width // 4, screen.height - 100))
+    # screen.screen.blit(button_return, button_return_rect)
+
+    # french_button.draw(TEXT_COLOR)
+    # english_button.draw(TEXT_COLOR)
+    button_list = [easy_difficulty_button, normal_difficulty_button, nightmare_difficulty_button]
+    language_list = [french_button, english_button]
     mouse_position = pygame.mouse.get_pos()
     for event in pygame.event.get():
         for button in button_list:
             if button.rect.collidepoint(mouse_position):
-                button.hovered = True
-                button.draw("red")
+                # new_background = screen.background(BACKGROUND_IMAGE_MENU, "Fruits Slicer - Scores")
+                # screen.screen.blit(new_background, (0,0))
+                # create_mode_menu_button()
+                button.text_render(STYLE_FONT, "red")
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     game_mode = button.identification
+            else:
+
                     
         for language in language_list:
             if language.rect.collidepoint(mouse_position):
                 language.hovered = True
-                language.draw("red")
+                # language.color = "red"
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     language = language.identification
 
