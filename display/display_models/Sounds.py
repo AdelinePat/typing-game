f10aaffacaebe92e253739692f11245f923347ad
enolaworld <enolaworld@gmail.com>
2025-02-04 18:42:07 +0100

    feat: change channel sound bomb

--- display/display_models/Sounds.py
+++ display/display_models/Sounds.py
@@ -2,8 +2,9 @@ import pygame
 
 class Sounds:
     def __init__(self):
-        """loading sound effects"""
         pygame.mixer.init()
+
+        # Chargement des effets sonores
         self.slice_sound = pygame.mixer.Sound("./assets/sounds/slice_01.wav")
         self.slice_sound.set_volume(0.5)
         self.bomb_sound = pygame.mixer.Sound("./assets/sounds/explosion-2.wav")
@@ -11,19 +12,21 @@ class Sounds:
         self.freeze_sound = pygame.mixer.Sound("./assets/sounds/freeze.wav")
         self.freeze_sound.set_volume(0.5)
         self.game_over_sound = pygame.mixer.Sound("./assets/sounds/game_over.wav")
-        # loading background music
-        self.background_music = "./assets/sounds/ninja.mp3"
 
-        # Init canals
-        self.music_channel = pygame.mixer.Channel(0)  # canal dedicated to background music
-        self.effect_channel = pygame.mixer.Channel(1)  # canal dedicated to other sound effects
+        # Chargement de la musique de fond
+        self.background_music = "./assets/sounds/ninja.mp3"  # Chemin vers votre fichier MP3
 
-    # Sound effects
+        # Initialisation des canaux
+        self.music_channel = pygame.mixer.Channel(0)  # Canal dédié à la musique de fond
+        self.effect_channel = pygame.mixer.Channel(1)  # Canal dédié aux effets sonores généraux
+        self.bomb_channel = pygame.mixer.Channel(2)  # Canal dédié au son de la bombe
+
+    # Effets sonores
     def play_slice_sound(self):
         self.effect_channel.play(self.slice_sound)
 
     def play_bomb_sound(self):
-        self.effect_channel.play(self.bomb_sound)
+        self.bomb_channel.play(self.bomb_sound)
 
     def play_freeze_sound(self):
         self.effect_channel.play(self.freeze_sound)
@@ -31,11 +34,11 @@ class Sounds:
     def play_game_over_sound(self):
         self.effect_channel.play(self.game_over_sound)
 
-    # Background music
+    # Musique de fond
     def play_background_music(self, volume=0.1):
         pygame.mixer.music.load(self.background_music)
-        pygame.mixer.music.set_volume(volume)  # Adjust volume (0.0 to 1.0)
-        pygame.mixer.music.play(-1)  # Music loop
+        pygame.mixer.music.set_volume(volume)  # Ajuste le volume (0.0 à 1.0)
+        pygame.mixer.music.play(-1)  # Joue la musique en boucle
 
     def stop_background_music(self):
         pygame.mixer.music.stop()
